cfeec74bc0d6090f7c1259376710416ab9482dca
Loading8210 <mcjzz1@outlook.com>
2026-05-01 10:27:54 +0800

    update 一元二次求根.py

--- PythonPrograms/一元二次求根.py
+++ PythonPrograms/一元二次求根.py
@@ -11,14 +11,16 @@ while 1:
         if user_choice == "y":
             if a == 0:
                 print("不是二元一次方程!")
+                break
             elif b**2-4*a*c < 0:
                 print("无实数解!")
+                break
             if b**2-4*a*c == 0:
                 print(f"此方程的解为：x1 = x2 = {(-b+(b**2-4*a*c)**0.5)/2*a}")
             if b**2-4*a*c > 0:
                 x1 = (-b+(b**2-4*a*c)**0.5)/2*a
                 x2 = (-b-(b**2-4*a*c)**0.5)/2*a
-                print(f"此方程的解为：x1 = {x1}，x2 = {x2}")
+                print(f"此方程的解为：x1 = {x1}或x2 = {x2}")
                 break
         elif user_choice == "n":
             break
